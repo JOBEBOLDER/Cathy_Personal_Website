a496f2f546ddd22d57e094cb322e7da964fc6ce1
jieyaochen <chenjieyao0824@gmail.com>
2025-07-21 12:38:26 -0400

    deploy fix jason

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
-import { ThemeProvider } from './contexts/ThemeContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
+   
           {children}
-        </ThemeProvider>
+    
       </body>
     </html>
   )
